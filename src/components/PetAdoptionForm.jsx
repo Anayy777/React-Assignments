c2b870ac24cad8e388df11b85ebc08ddc4addac7
Anayy777 <777.anaycse@gmail.com>
2025-08-20 18:15:05 +0000

    first commit

---
+++ src/components/PetAdoptionForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PetAdoptionForm = () => {
+  return (
+    <div style={{
+      fontFamily : "sans-serif" , 
+      color : "grey" , 
+      fontSize : 25 , 
+      paddingTop : 10 , 
+      backgroundColor: "#c59771bd"
+    }}>Pet Adoption Form</div>
+  )
+}
+
+export default PetAdoptionForm
